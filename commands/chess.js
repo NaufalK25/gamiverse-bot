8ee7adf5acd0f229eb582cf5be911f86db66b085
NaufalK25 <naufalkateni2001@gmail.com>
2023-02-12 12:01:16 +0700

    add chess.com

---
+++ commands/chess.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { addField, humanizeDate } = require('../helpers');
+
+const CHESS_THUMBNAIL = 'https://res.cloudinary.com/dko04cygp/image/upload/v1676176840/gamiverse/chess/chess_xopl8k.png';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('chess')
+        .setDescription('Get the player profile from Chess.com')
+        .addStringOption(option => option.setName('username').setDescription('The username of the player').setRequired(true)),
+    async execute(interaction) {
+        try {
+            const argUsername = interaction.options.getString('username').trim().toLowerCase();
+            const res = await fetch(`https://api.chess.com/pub/player/${argUsername}`);
+            const player = await res.json();
+
+            if (player.code === 0) {
+                const embed = new EmbedBuilder()
+                    .setColor('#FFCCCC')
+                    .setTitle('Error')
+                    .setThumbnail(CHESS_THUMBNAIL)
+                    .setDescription(`Player with username \`${argUsername}\` doesn't exist`)
+                    .setFooter({ text: 'Chess.com' });
+
+                return interaction.reply({ embeds: [embed] });
+            }
+
+            const countryRes = await fetch(player.country);
+            const country = await countryRes.json();
+
+            const embed = new EmbedBuilder()
+                .setColor('#779556')
+                .setTitle(player.username)
+                .setURL(player.url)
+                .setThumbnail(CHESS_THUMBNAIL)
+                .addFields(
+                    addField('Followers', player.followers),
+                    addField('Country', country.name || 'None'),
+                    addField('\u200B', '\u200B', false, { highlight: false }),
+                    addField('Status', player.status),
+                    addField('League', player.league || 'None'),
+                    addField('\u200B', '\u200B', false, { highlight: false }),
+                    addField('Last Online', humanizeDate(player.last_online)),
+                    addField('Member Since', humanizeDate(player.joined))
+                )
+                .setFooter({ text: 'Chess.com' });
+
+            interaction.reply({ embeds: [embed] });
+        } catch (err) {
+            const embed = new EmbedBuilder()
+                .setColor('#FFCCCC')
+                .setTitle('Error')
+                .setThumbnail(CHESS_THUMBNAIL)
+                .setDescription(
+                    [
+                        'This error can be caused by:',
+                        '1. Rate limit exceeded',
+                        '2. Internal server error',
+                        '3. Server is under maintenance',
+                        'Please contact the developer if the error persists.'
+                    ].join('\n')
+                )
+                .setFooter({ text: 'Chess.com' });
+
+            interaction.reply({ embeds: [embed] });
+        }
+    }
+};
--- helpers.js
+++ helpers.js
@@ -1,9 +1,39 @@
-const generateField = (name, value, inline = true, option = { highlight: true }) => ({
+const addField = (name, value, inline = true, option = { highlight: true }) => ({
     name,
     value: option.highlight ? `\`${value}\`` : `${value}`,
     inline
 });
 
+const humanizeDate = unixTimestamp => {
+    const date = new Date(unixTimestamp * 1000);
+
+    const currentDate = new Date();
+    const diffInMilliseconds = currentDate - date;
+    const diffInSeconds = diffInMilliseconds / 1000;
+    const diffInMinutes = diffInSeconds / 60;
+    const diffInHours = diffInMinutes / 60;
+    const diffInDays = diffInHours / 24;
+    const diffInMonths = diffInDays / 30.44;
+    const diffInYears = diffInDays / 365.25;
+
+    if (diffInYears >= 1) {
+        return `${Math.floor(diffInYears)} year${diffInYears >= 2 ? 's' : ''} ago`;
+    } else if (diffInMonths >= 1) {
+        return `${Math.floor(diffInMonths)} month${diffInMonths >= 2 ? 's' : ''} ago`;
+    } else if (diffInDays >= 1) {
+        return `${Math.floor(diffInDays)} day${diffInDays >= 2 ? 's' : ''} ago`;
+    } else if (diffInHours >= 1) {
+        return `${Math.floor(diffInHours)} hour${diffInHours >= 2 ? 's' : ''} ago`;
+    } else if (diffInMinutes >= 1) {
+        return `${Math.floor(diffInMinutes)} minute${diffInMinutes >= 2 ? 's' : ''} ago`;
+    } else if (diffInSeconds >= 1) {
+        return `${Math.floor(diffInSeconds)} second${diffInSeconds >= 2 ? 's' : ''} ago`;
+    } else {
+        return 'Just now';
+    }
+};
+
 module.exports = {
-    generateField
+    addField,
+    humanizeDate
 };
