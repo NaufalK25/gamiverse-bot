4a76530c5e19559375c1576898b3bda7fda283c1
NaufalK25 <naufalkateni2001@gmail.com>
2023-02-15 19:20:42 +0700

    add telegram bot version

---
+++ telegram/commands/chess.js
@@ -0,0 +1,34 @@
+const { humanizeDate, nodeFetch } = require('../helpers');
+
+const chessCommand = bot => {
+    bot.onText(/\/chess (.+)/, async (msg, match) => {
+        const chatId = msg.chat.id;
+        const username = match[1].trim().toLowerCase();
+
+        const player = await nodeFetch(`https://api.chess.com/pub/player/${username}`);
+
+        if (player.code === 0) {
+            return bot.sendMessage(chatId, `Sorry, we couldn't find a player with the username <b>${username}</b>. Please check that you have entered the correct username and try again`, {
+                parse_mode: 'HTML'
+            });
+        }
+
+        const country = await nodeFetch(player.country);
+
+        const text = [
+            `<b>Username:</b> <a href="${player.url}">${player.username}</a>`,
+            `<b>Followers:</b> ${player.followers}`,
+            `<b>Country:</b> ${country.name || 'None'}`,
+            `<b>Status:</b> ${player.status}`,
+            `<b>League:</b> ${player.league || 'None'}`,
+            `<b>Last Online:</b> ${humanizeDate(new Date(player.last_online * 1000))}`,
+            `<b>Member Since:</b> ${humanizeDate(new Date(player.joined * 1000))}`
+        ];
+
+        bot.sendMessage(chatId, text.join('\n'), {
+            parse_mode: 'HTML'
+        });
+    });
+};
+
+module.exports = chessCommand;
